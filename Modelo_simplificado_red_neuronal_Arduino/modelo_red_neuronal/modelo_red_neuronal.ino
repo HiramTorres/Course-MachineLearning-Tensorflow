e95737748a57a068118a5abb8420c1f9c420724e
Hiram Torres <hiram.ht10@gmail.com>
2021-03-26 21:42:52 -0600

    first commit

---
+++ Modelo_simplificado_red_neuronal_Arduino/modelo_red_neuronal/modelo_red_neuronal.ino
@@ -0,0 +1,23 @@
+double p1 = 0.51;
+double p2 = 0.59;
+double p3 = -0.15;
+
+double w11 = 0.57;
+double w12 = -0.79;
+double w13 = 0.75;
+double b = 0.8;
+
+double n = 0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  ///////////////////////////// Red neuronal de entradas múltiples //////////////////////
+  n = (w11*p1)+(w12*p2)+(w13*p3)+b;
+  Serial.print("Salida n: ") ;
+  Serial.println(n,4) ;
+}
+
+void loop() {
+
+}
